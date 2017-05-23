42fd7d8efdd68a8bafe84093ef8e18da69765354
bernhardtandy <bernhardtandy@yahoo.com>
2017-05-23 13:41:46 -0400

    Add files via upload

---
+++ Wall.java
@@ -0,0 +1,49 @@
+//Walls
+import java.awt.*;
+public class Wall
+{
+	//Direction Numbers
+	//Right 1, Up 2, Left 3, Down 4
+	int x, y, xsize, ysize;
+	boolean inwall;
+	public Wall(int xe, int ye, int xsizee, int ysizee)
+	{
+		x = xe;
+		y = ye;
+		xsize = xsizee;
+		ysize = ysizee;
+	}
+	public void drawWall(Graphics g)
+	{
+		g.setColor(Color.BLACK);
+		g.fillRect(x, y, xsize, ysize);
+	}
+	public int getX()
+	{
+		return x;
+	}
+	public int getY()
+	{
+		return y;
+	}
+	public int getXSize()
+	{
+		return xsize;
+	}
+	public int getYSize()
+	{
+		return ysize;
+	}
+	public boolean checkBoundaries(int playerx, int playery)
+	{
+		if ((playerx + 30 > x) && (playerx < x + xsize) && (playery + 30 > y) && (playery < y + ysize))
+		{
+			inwall = true;
+		}
+		else
+		{
+			inwall = false;
+		}
+		return inwall;
+	}
+}
